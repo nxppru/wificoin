4a658b1542aaf2bec055a419aee2f638ab346d0e
liudf0716 <liudf0716@gmail.com>
2018-02-01 10:40:55 +0800

    liudf tuning parameters

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -79,7 +79,7 @@ public:
         consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8");
         consensus.BIP65Height = 388381; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
         consensus.BIP66Height = 363725; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
-        consensus.powLimit = uint256S("00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.powLimit = uint256S("000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 1.4 * 24 * 60 * 60; // 1.4 day
         consensus.nPowTargetSpacing =   60;
         consensus.fPowAllowMinDifficultyBlocks = false;
@@ -182,7 +182,7 @@ public:
         consensus.BIP34Hash = uint256S("0x0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8");
         consensus.BIP65Height = 581885; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 330776; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
-        consensus.powLimit = uint256S("00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.powLimit = uint256S("000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 1.4 * 24 * 60 * 60; // 1.4 day
         consensus.nPowTargetSpacing = 60;
         consensus.fPowAllowMinDifficultyBlocks = true;
@@ -218,7 +218,7 @@ public:
         nDefaultPort = 18666;
         nPruneAfterHeight = 1000;
 
-	genesis = CreateGenesisBlock(1517196798, 394770664, 0x1d00ffff, 1, 50 * COIN);
+		genesis = CreateGenesisBlock(1517196798, 394770664, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x0000000042866c964bc494120a600f8aabdad6e1f205d28f0a1ee2470b6a1440"));
         assert(genesis.hashMerkleRoot == uint256S("0xa27472e72d119144cee58b1ee490898fe00ead37cb0792292cb33a860a342dcb"));
@@ -227,10 +227,10 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
 		
-	vSeeds.emplace_back("wifidog.kunteng.org", false); // liudf for test 
+		vSeeds.emplace_back("wifidog.kunteng.org", false); // liudf for test 
         vSeeds.emplace_back("emqtt.kunteng.org", false); // liudf for test 
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,95);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
